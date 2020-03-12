1d39d039655b9356b6621d03a637880c59554e39
ramlaxman0427 <48586953+ramlaxman0427@users.noreply.github.com>
2020-03-12 12:40:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,21 +64,6 @@ resource "aws_route_table" "terraform-public" {
         Name = "${var.Main_Routing_Table}"
     }
 }
-
-###############################
-
-route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.default.id}"
-    }
-
-    tags = {
-        Name = "${var.Main_Routing_Table}"
-    }
-}
-
-
-############################################
 resource "aws_route_table_association" "terraform-public" {
     subnet_id = "${aws_subnet.subnet1-public.id}"
     route_table_id = "${aws_route_table.terraform-public.id}"
